5304a55dafb8b50abc87bb6be503a22dc0168c1f
Ярослав Масенко <chasodij@outlook.com>
2022-04-20 22:16:58 +0300

    Трохи скоротив код. Може щось читатиметься складно, але коменти ще не додавав.

--- classes/BasketPage.php
+++ classes/BasketPage.php
@@ -5,60 +5,43 @@ class BasketPage extends Page{
     }
     
     function getMain() : string{
+        session_start();
+
         $returnString = '<main>
                     <div>ВИ В КОШИКУ (СМІТНИКУ, ХАХА)</div>
                     <div class="container">';
-        session_start();
-        if (isset($_SESSION["1_check"]) && $_SESSION["1_check"] == "on"){
-            $num_1 = 0;
-            if (isset($_SESSION["1_num"])) {
-                $num_1  = $_SESSION["1_num"];
-            }
-            $returnString .='<div class="row align-items-center">
-                                <div class="col-3">
-                                    <img src="/img/firebird.jpg" alt="Firebird picture">
-                                </div>
-                                <div class="col-6">
-                                firebird
-                                </div>
-                                <div class="col-3">
-                                    '.$num_1.'
-                                </div>
-                            </div>';
+
+        $products = [["firebird", "/img/firebird.jpg"], ["ganzo", "/img/ganzo.jpg"], ["truper", "/img/truper.jpg"]];
+
+        if (count($_SESSION) <= count($products)){
+            return $returnString.'<a href="/index.php">Перейти до покупок</a></div></main>';
         }
-        if (isset($_SESSION["2_check"]) && $_SESSION["2_check"] == "on"){
-            $num_2 = 0;
-            if (isset($_SESSION["2_num"])) {
-                $num_2  = $_SESSION["2_num"];
-            }
-            $returnString .='<div class="row align-items-center">
+
+        for ($i = 0; $i < 3; $i++){
+
+
+            $i_check = $i."_check";
+            $i_num = $i."_num";
+
+            if (isset($_SESSION[$i_check]) && $_SESSION[$i_check] == "on"){
+
+                $num = 0;
+                if (isset($_SESSION[$i_num])) {
+                    $num  = $_SESSION[$i_num];
+                }
+
+                $returnString .='<div class="row align-items-center">
                                 <div class="col-3">
-                                    <img src="/img/ganzo.jpg" alt="Ganzo picture">
+                                    <img src="'.$products[$i][1].'" alt="'.$products[$i][0].' picture">
                                 </div>
                                 <div class="col-6">
-                                ganzo
+                                '.$products[$i][0].'
                                 </div>
                                 <div class="col-3">
-                                    '.$num_2.'
+                                    '.$num.'
                                 </div>
                             </div>';
-        }
-        if (isset($_SESSION["3_check"]) && $_SESSION["3_check"] == "on"){
-            $num_3 = 0;
-            if (isset($_SESSION["3_num"])) {
-                $num_3  = $_SESSION["3_num"];
             }
-            $returnString .='<div class="row align-items-center">
-                                <div class="col-3">
-                                    <img src="/img/truper.jpg" alt="Truper picture">
-                                </div>
-                                <div class="col-6">
-                                truper
-                                </div>
-                                <div class="col-3">
-                                    '.$num_3.'
-                                </div>
-                            </div>';
         }
         return $returnString.'</div></main>';
     }
