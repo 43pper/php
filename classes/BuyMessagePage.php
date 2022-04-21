e189453c88f9152d370096f8ca17917b72b4fcb0
Ярослав Масенко <chasodij@outlook.com>
2022-04-21 21:45:35 +0300

    Дурачок букву не ту написав

--- classes/BuyMessagePage.php
+++ classes/BuyMessagePage.php
@@ -6,7 +6,7 @@ class BuyMessagePage extends Page{
 
     function getMain() : string{
         return '<main class="bg-light">
-                    <div class="container bg-white body_content"><h2>Покупку успішно додана до кошика</h2><br>
+                    <div class="container bg-white body_content"><h2>Покупку успішно додано до кошика</h2><br>
                     <a href="/basket.php" class="link-primary">Перейти в кошик</a>
                     </div>';
 
