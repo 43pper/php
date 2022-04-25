871f4c65a76c09fcddfa976f9222709d74cc4d20
Stanislav Matsak <stanislav.matsak@nure.ua>
2022-04-26 00:32:35 +0300

    бля кинув звіт в гітхаб внатурі

--- delete.php
+++ delete.php
@@ -7,10 +7,10 @@ spl_autoload_register("autoload");
 
 session_start();
 
-$_POST = json_decode(file_get_contents('php://input'), true);
+$deletedItem = json_decode(file_get_contents('php://input'), true);
 
-$_SESSION[$_POST["id"]."_check"] = "off";
-$_SESSION[$_POST["id"]."_num"] = 0;
+$_SESSION[$deletedItem["id"]."_check"] = "off";
+$_SESSION[$deletedItem["id"]."_num"] = 0;
 
 $page = new BasketPage();
 $page->getPage();
